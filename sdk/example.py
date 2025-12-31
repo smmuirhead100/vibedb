444454e69bf01211cd46e6ee8b03fd14b380d57c
Sean Muirhead <smmuirhead100@berkeley.edu>
2025-12-31 10:04:30 -0800

    add example of what the client should look like

---
+++ sdk/example.py
@@ -0,0 +1,19 @@
+# from vibedb.sdk import Client
+
+
+# # 1. Instantiate the client
+# client = Client()
+
+# # 2. Send some data
+# client.execute("New Event: new user signed up with first name 'Sean' and last name 'Muirhead'. Phone number is 555-555-1234.")
+# client.execute("Sean Muirhead changed their phone number from 555-555-1234 to 555-555-5555")
+# client.execute("Add these colors of shoes: red, green, blue")
+# client.execute("New Event: new user signed up with first name 'Bob' and last name 'Test'. No phone number.")
+# client.execute("New Event: Bob Test just added an email to their account: bob@bobby.com")
+
+# # 3. Query the data
+# client.execute("Get all users who've signed up in the past day.")
+# client.execute("What are the colors of shoes?")
+
+# # 4. Query and cast the data
+# client.execute("Get all users who've signed up in the past day", User)
