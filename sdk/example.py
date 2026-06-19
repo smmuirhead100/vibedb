f24b0d73288f300cecbb1e2fc9c4e8f0033e7c56
Sean Muirhead <smmuirhead100@berkeley.edu>
2026-06-19 10:30:50 -0700

    simplify example

--- sdk/example.py
+++ sdk/example.py
@@ -25,23 +25,6 @@ async def run():
     users_again = await client.execute("Get last 1 users created in descending order by created_at", return_as=List[User])
     print(f"Users again: {users_again}")
 
-    # await client.execute("New Event: new user signed up with first name 'Bob' and last name 'Test'. Phone number is 555-555-5555.")
-    # await client.execute("Sean Muirhead changed their phone number from 555-555-1234 to 555-555-5555")
-    # await client.execute("Add these colors of shoes: red, green, blue")
-    # await client.execute("New Event: new user signed up with first name 'Bob' and last name 'Test'. No phone number.")
-    # await client.execute("New Event: Bob Test just added an email to their account: bob@bobby.com")
-
-    # # 2. Create a new client and query the data
-    # client_2 = Client()
-    # users = await client_2.execute("Get all users who've signed up in the past day.")
-    # print("Users: ", users)
-
-    # colors = await client_2.execute("Get all colors of shoes.")
-    # print("Colors: ", colors)
-
-    # 3. Query and cast the data
-    # client.execute("Get all users who've signed up in the past day", User)
-
 
 if __name__ == "__main__":
     import asyncio
